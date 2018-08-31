ceda7917e925a90aa848e07bf3fa49406e573aa3
aluno <aluno@10.32.18.239>
2018-08-31 09:08:07 -0300

    Hands On

--- Carros/src/br/gov/serpro/curso/Principal.java
+++ Carros/src/br/gov/serpro/curso/Principal.java
@@ -1,5 +1,18 @@
 package br.gov.serpro.curso;
 
 public class Principal {
+	public static void main(String [] args) {
+		Carros c1 = new Carros();
+		c1.potencia = 10;
+		c1.nome = "Punto";
+		c1.velocidade = 0;
+
+		c1.acelerar();
+		c1.acelerar();
+		c1.acelerar();
+		c1.frear();
+
+		c1.imprimir();
+		}
 
 }
