6bd8c89b8ab4b27d55da489c9bc1e27ed6929458
aluno <aluno@10.32.18.239>
2018-09-17 10:54:35 -0300

    Carros Teste

--- Carros/Teste/br/gov/serpro/curso/TesteCarros.java
+++ Carros/Teste/br/gov/serpro/curso/TesteCarros.java
@@ -1,45 +1,53 @@
 package br.gov.serpro.curso;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.junit.jupiter.api.Test;
+import static org.junit.Assert.assertEquals;
 
-import br.gov.serpro.curso.Carros;
+import org.junit.Before;
+import org.junit.Test;
 
 public class TesteCarros {
+	
+	private Carros carros;
+	
+	@Before
+	public void before() {
+		carros = new Carros();
+		carros.potencia = 10;
+	}
     
     @Test
     public void testCarroParado() {
-        Carros c = new Carros();       
-        assertEquals(0,c.getVelocidade());
+//        Carros c = new Carros();       
+        assertEquals(0,carros.getVelocidade());
        }
     
     @Test
     public void testAcelerar() {
-        Carros c = new Carros();
-        c.potencia = 10;
-        c.acelerar();
-        assertEquals(10,c.getVelocidade());
+        /*Carros c = new Carros();
+        c.potencia = 10;*/
+        carros.acelerar();
+        assertEquals(10,carros.getVelocidade());
        }
     
     @Test
     public void testFrear() {
-        Carros c = new Carros();
-        c.potencia = 10;
-        c.acelerar();
-        c.frear();
-        assertEquals(5,c.getVelocidade());
+        //Carros c = new Carros();
+        //carros.potencia = 10;
+        carros.acelerar();
+        carros.frear();
+        assertEquals(5,carros.getVelocidade());
        }
     
     @Test
     public void testFrearAteZero() {
-        Carros c = new Carros();
-        c.potencia = 10;
-        c.acelerar();
-        c.frear();
-        c.frear();
-        c.frear();
-        c.frear();
-        assertEquals(0,c.getVelocidade());
+        //Carros c = new Carros();
+        //carros.potencia = 10;
+        carros.acelerar();
+        carros.frear();
+        carros.frear();
+        carros.frear();
+        carros.frear();
+        assertEquals(0,carros.getVelocidade());
        } 
     /// teste
 }
