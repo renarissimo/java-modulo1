8117b5a9d4efbe3b631942ce747aba3cd3c89e9d
aluno <aluno@10.32.18.239>
2018-10-29 10:58:58 -0300

    commit autoridade

--- Tratamento/src/serpro/Autoridade.java
+++ Tratamento/src/serpro/Autoridade.java
@@ -1,5 +1,18 @@
 package serpro;
 
-public class Autoridade {
+public class Autoridade implements FormatadorNome {
+	String nome;
+	String sobrenome;
+	
+	public String getTratamento() {
+		
+	}
+	@Override
+	public String formatarNome(String nome, String sobrenome) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	
 
 }
